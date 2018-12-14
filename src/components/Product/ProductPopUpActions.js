faa5683d14b071ec1b268f1bd451cba69c7a6f97
Samuel Garcia <samuelgzrte@gmail.com>
2018-12-14 11:21:52 -0600

    Added Add to cart Icem on home

--- src/components/Product/ProductPopUpActions.js
+++ src/components/Product/ProductPopUpActions.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import StoreFronted from '../../StoreFrontend.context';
 
 const ProductPopUpActions = ({ id }) => (
     <div className="action-btns">
         <Link to={`/plates/${id}`}><button>View Details</button></Link>
         <br />
-        <button>Add to Cart</button>
-    </div>
+        <StoreFronted.Consumer>
+            {
+                ({ addItemToCart }) => {
+                    return <button onClick={() => addItemToCart(id, 1)}> Add to Cart</button>
+                }
+
+            }
+        </StoreFronted.Consumer>
+    </div >
 );
 
 export default ProductPopUpActions;
