268595d0290a605a2ba672635f2d03b5c9ab5b73
Samuel Garcia <samuelgzrte@gmail.com>
2018-12-14 17:05:35 -0600

    Added unit price

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import StoreFrontend from '../../StoreFrontend.context';
+import ProductPrice from '../Product/ProductPrice';
 
 const CartItem = ({ item }) => (
     <div className="cart-item-container">
@@ -19,11 +20,14 @@ const CartItem = ({ item }) => (
                 {({ getTotal, removeAll, getQuantity, updateRootQuantity }) => {
                     return (
                         <React.Fragment>
-                            <div className="cart-item-quantity">                                
+                            <div className="cart-item-quantity">
                                 <input type="number" min="1" step="1" value={getQuantity(item.id)} onChange={(e) => updateRootQuantity(item.id, e.target.value)} />
                             </div>
                             <div className="cart-item-remove">
                                 <span onClick={() => removeAll(item.id)}>&#10006;</span>
+                            </div>
+                            <div className="cart-item-price">
+                                <ProductPrice price={item.price} />
                             </div>
                         </React.Fragment>
                     )
