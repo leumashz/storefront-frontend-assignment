46439aca7ba799f036d561a445103a0fac4cc25e
Samuel Garcia <samuelgzrte@gmail.com>
2018-12-13 18:33:54 -0600

    Added style changes

---
+++ src/components/CategoryView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PrincipalMessage from './PrincipalMessage';
+import MainContainer from './MainContainer';
+
+const CategoryView = () => {
+    return (
+        <React.Fragment>
+            <PrincipalMessage />
+            <MainContainer />
+        </React.Fragment>
+    );
+};
+
+export default CategoryView;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './Header.css';
+import logo from '../logo.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <React.Fragment>
     <div>
-      <div className="store-name">Hero</div>
+      <div className="store-name"><img src={logo} alt="hero-logo"></img></div>
       <div className="store-menu-section">
         <ul className="menu-hero-header">
-          <li>HOME</li>
+          <li>
+            <Link to="/">
+              HOME
+            </Link>
+          </li>
           <li>SHOP</li>
           <li>JOURNAL</li>
           <li>MORE</li>
